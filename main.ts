3395e32eac6ba14c431025dc1b56baba7f58b959
hczelnxt <115052305+hczelnxt@users.noreply.github.com>
2022-10-05 09:42:42 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    count = 1
+    for (let index = 0; index < 6; index++) {
+        basic.showNumber(count)
+        basic.pause(1000)
+        count += 1
+    }
 })
+input.onButtonPressed(Button.B, function () {
+    count = 9
+    for (let index = 0; index < 10; index++) {
+        basic.showNumber(count)
+        basic.pause(1000)
+        count += -1
+    }
+})
+let count = 0
+basic.showString("2D07 HAZEL")
