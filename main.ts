1e28a72cd93b1a5ebe8528f0f8acde278ba76a19
hczelnxt <115052305+hczelnxt@users.noreply.github.com>
2022-10-12 09:21:20 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,56 @@
 input.onButtonPressed(Button.A, function () {
-    count = 1
-    for (let index = 0; index < 6; index++) {
-        basic.showNumber(count)
-        basic.pause(1000)
-        count += 1
-    }
+    count = -1
 })
 input.onButtonPressed(Button.B, function () {
-    count = 9
-    for (let index = 0; index < 10; index++) {
+    count = 10
+    while (count >= 0) {
         basic.showNumber(count)
         basic.pause(1000)
         count += -1
     }
+    basic.pause(2000)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . . . .
+        # . . . #
+        . . . . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
 })
 let count = 0
 basic.showString("2D07 HAZEL")
